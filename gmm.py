4c69ae7c94d5075061502f99394a1419edcf28cc
Nalin Das <nalindas9@gmail.com>
2020-03-27 10:59:08 -0400

    Add header and comments

--- gmm.py
+++ gmm.py
@@ -1,8 +1,17 @@
+"""
+Expectation Maximization Algorithm
+
+Authors:
+Nalin Das (nalindas9@gmail.com)
+Graduate Student pursuing Masters in Robotics,
+University of Maryland, College Park
+"""
+
 import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 
-#Plotting RGB channel histograms
+# Plotting RGB channel histograms and observing the value of K from this
 img = cv2.imread('/home/nalindas9/Documents/Courses/Spring_2020_Semester_2/ENPM673_Perception_for_Autonomous_Robots/Github/project3/Color-segmentation-using-GMM/Green_boi_42.png')
 color = ('b','g','r')
 for i,col in enumerate(color):
@@ -10,4 +19,9 @@ for i,col in enumerate(color):
   plt.plot(histr,color = col)
   plt.xlim([0,256])
   plt.show()
+  
+# K is the number of Gaussians that can be fit into the histogram
+# For the image Green_boi_42.png, K is 5
+
+
 
